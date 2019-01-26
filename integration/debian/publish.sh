7ea4dcebcdc6c0507dec1be3b0bfad8af43dd60f
Elvis Pranskevichus <elvis@magic.io>
2019-01-26 18:48:30 -0500

    Tighten package upload security

--- integration/debian/publish.sh
+++ integration/debian/publish.sh
@@ -6,18 +6,27 @@ mkdir -p "${HOME}/.ssh" && chmod 700 "${HOME}/.ssh"
 echo "${DPUT_SSH_KEY}" > "${HOME}/.ssh/id_ed25519"
 chmod 400 "${HOME}/.ssh/id_ed25519"
 
+cat <<EOF >"${HOME}/.ssh/config"
+Host upload-packages.prod.edgedatabase.net
+    Port 2222
+    StrictHostKeyChecking no
+EOF
+
 set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y dput
+apt-get install -y dput python3-paramiko
 
-dput -d -d artifacts/*.changes \
-    --override "fqdn=upload-packages.prod.edgedatabase.net:2222" \
-    --override "method=sftp" \
-    --override "full_upload_log=true" \
-    --override "incoming=/data/apt/incoming/" \
-    --override "login=root" \
-    --override "allow_unsigned_uploads=true" \
-    --override 'post_upload_command="ssh -p2222 root@upload-packages.prod.edgedatabase.net -- reprepro --verbose processincoming"'
+cat <<EOF >/tmp/dput.cf
+[edgedb-prod]
+fqdn                    = upload-packages.prod.edgedatabase.net
+login                   = uploader
+allow_dcut              = 1
+method                  = sftp
+allow_unsigned_uploads  = 1
+post_upload_command     = post_upload_command="ssh reprepro@upload-packages.prod.edgedatabase.net -- /usr/bin/reprepro processincoming"
+EOF
+
+dput -d -d -c /tmp/dput.cf edgedb-prod artifacts/*.changes
