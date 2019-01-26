08d100a9aa8ed9389d69c5046f68aaa57e6ef774
Elvis Pranskevichus <elvis@magic.io>
2019-01-25 21:52:12 -0500

    Switch to simple output mode

--- integration/debian/test.sh
+++ integration/debian/test.sh
@@ -6,6 +6,6 @@ apt-get update
 dpkg -i artifacts/edgedb-server_*_amd64.deb || apt-get -f install -y
 su edgedb -c '/usr/lib/x86_64-linux-gnu/edgedb-server/bin/python3 \
               -m edb.tools --no-devmode test /usr/share/edgedb-server/tests \
-              -e flake8 -e sphinx --output-format=verbose'
+              --output-format=simple'
 systemctl enable --now edgedb-0
 "[[ $(echo 'SELECT 1 + 3;' | edgedb -u edgedb) == *4* ]]"
