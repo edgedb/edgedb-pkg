adf69e72b1a8bf31572ac30c08ebb994e6d72f94
Elvis Pranskevichus <elvis@magic.io>
2019-01-26 14:35:12 -0500

    Use dput for package uploads

--- integration/debian/publish.sh
+++ integration/debian/publish.sh
@@ -5,4 +5,12 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y reprepro
+apt-get install -y dput
+
+dput -d -d artifacts/*.changes \
+    --override "fqdn=upload-packages.prod.edgedatabase.net:2222" \
+    --override "method=sftp" \
+    --override "full_upload_log=true" \
+    --override "incoming=/data/apt/incoming/" \
+    --override "login=root" \
+    --override 'post_upload_command="ssh -p2222 root@upload-packages.prod.edgedatabase.net -- reprepro --verbose processincoming"'
