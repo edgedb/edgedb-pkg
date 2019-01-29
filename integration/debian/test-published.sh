dc1db38e94571f3297f0d238ae590cc4b1fff979
Elvis Pranskevichus <elvis@magic.io>
2019-01-28 23:34:43 -0500

    Fix test-published

--- integration/debian/test-published.sh
+++ integration/debian/test-published.sh
@@ -9,7 +9,7 @@ apt-get install -y curl gnupg
 curl https://packages.prod.edgedatabase.net/keys/edgedb.asc | apt-key add -
 echo deb [arch=amd64] https://packages.prod.edgedatabase.net/apt ${DISTRO} main \
     >> /etc/apt/sources.list.d/edgedb.list
+apt-get update
 apt-get install -y edgedb-server
-dpkg -i artifacts/edgedb-server_*_amd64.deb || apt-get -f install -y
 edgedb-server --help
 edgedb --help
