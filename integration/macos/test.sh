b92576fc20474d97423dfd69128ba690ef672147
Elvis Pranskevichus <elvis@magic.io>
2019-02-21 16:22:27 -0500

    Dump macOS installer log on failure

--- integration/macos/test.sh
+++ integration/macos/test.sh
@@ -2,13 +2,20 @@
 
 set -ex
 
-sudo installer -dumplog -verbose -pkg artifacts/*.pkg -target /Volumes/macOS
+sudo installer -dumplog -verbose \
+    -pkg artifacts/*.pkg \
+    -target /Volumes/macOS || (sudo cat /var/log/install.log && exit 1)
+
 source /etc/profile
+
 sudo su edgedb -c \
     '/Library/Frameworks/EdgeDB.framework/Versions/0/lib/edgedb-0/bin/python3 \
     -m edb.tools --no-devmode test \
     /Library/Frameworks/EdgeDB.framework/Versions/0/share/edgedb-0/tests \
     -e flake8 --output-format=simple'
+
 sudo launchctl print system/com.edgedb.edgedb-0
+
 [[ "$(echo 'SELECT 1 + 3;' | edgedb -u edgedb)" == *4* ]] || exit 1
+
 echo "Success!"
