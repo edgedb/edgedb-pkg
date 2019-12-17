cea70f6a5ae565a8d125787d4ef9d2dfa5cd84c3
Elvis Pranskevichus <elvis@edgedb.com>
2019-12-16 22:37:55 -0500

    Increase wait time on Centos
    Repo update on Centos may take considerably longer than 30s.

--- integration/containers/testpublished/entrypoint-centos.sh
+++ integration/containers/testpublished/entrypoint-centos.sh
@@ -23,8 +23,8 @@ gpgcheck=1
 gpgkey=https://packages.edgedb.com/keys/edgedb.asc
 EOF
 
-echo "Pausing for 30 seconds to let the package repo settle..."
-sleep 30
+echo "Pausing for 2 minutes to let the package repo settle..."
+sleep 120
 
 yum install -y edgedb-${slot}
 edgedb --help
