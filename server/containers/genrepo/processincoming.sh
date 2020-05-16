02d2fff40cd016f2cc35311e549b2abf7100bb64
Elvis Pranskevichus <elvis@edgedb.com>
2020-05-16 10:31:12 -0700

    genrepo: Make `gsutil rm` failure not-fatal

--- server/containers/genrepo/processincoming.sh
+++ server/containers/genrepo/processincoming.sh
@@ -58,8 +58,8 @@ done 10<"${list}"
 for dist in "${dists[@]}"; do
     gsutil ls "${basedir}/archive/${dist}*" \
         | grep -v "\(.sha256\|.asc\)$" \
-        | findold.py --keep=3 --subdist=nightly \
-        | gsutil -m rm -I
+        | findold.py --keep=3 --subdist=nightly
+        | gsutil -m rm -I || true
 
     gsutil ls "${basedir}/archive/${dist}*" \
         | sed -e "s|${basedir}/archive/||" \
