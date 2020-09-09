b2475cf3dd2f0c65de8451badd15726eeaab76c9
Łukasz Langa <lukasz@langa.pl>
2020-09-10 01:30:36 +0200

    Actually compile the changed .ts file

--- integration/actions/describe-artifact/lib/main.js
+++ integration/actions/describe-artifact/lib/main.js
@@ -1,15 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const process = __importStar(require("process"));
-const re = /^(?:\w+)-(\d+(-(dev|alpha|beta|rc)\d+)?).*\.(rpm|deb|img)$/gm;
+const re = /^(?:\w+)-(\d+(-(alpha|beta|rc)\d+)?(-dev\d+)?).*\.(rpm|deb|img|pkg)$/gm;
 async function run() {
     try {
         const target = process.env['INPUT_TARGET'];
