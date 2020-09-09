79cc9a322dd2dee0a879917b585fc8cc45af1277
Elvis Pranskevichus <elvis@edgedb.com>
2020-09-08 18:07:52 -0700

    Rudimentary Windows support
    This treats Windows as a Unix-ish target that has all the necessary GNU
    tools, Python, Git, Rust etc.
    
    Closes: #5

---
+++ integration/win/publish.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p "${HOME}/.ssh" && chmod 700 "${HOME}/.ssh"
+if [ -f "${PACKAGE_UPLOAD_SSH_KEY_FILE}" ]; then
+    cp "${PACKAGE_UPLOAD_SSH_KEY_FILE}" "${HOME}/.ssh/id_ed25519"
+else
+    echo "${PACKAGE_UPLOAD_SSH_KEY}" > "${HOME}/.ssh/id_ed25519"
+fi
+chmod 400 "${HOME}/.ssh/id_ed25519"
+
+set -ex
+
+cat <<EOF >"${HOME}/.ssh/config"
+Host upload-packages.edgedb.com
+    Port 2224
+    StrictHostKeyChecking no
+EOF
+
+dest="artifacts"
+key=""
+if [ -n "${PKG_PLATFORM}" ]; then
+    key+="-${PKG_PLATFORM}"
+fi
+if [ -n "${PKG_PLATFORM_VERSION}" ]; then
+    key+="-${PKG_PLATFORM_VERSION}"
+fi
+
+cd "${dest}"
+list=$(mktemp)
+batch=$(mktemp)
+find . -type f -print | sed "s|^\./||" > "${list}"
+chmod g+rw "${list}"
+
+cat <<EOF >${batch}
+put -r * incoming/
+put ${list} incoming/triggers/upload${key}.list
+EOF
+sftp -b "${batch}" uploader@upload-packages.edgedb.com
+
+rm "${list}" "${batch}"
