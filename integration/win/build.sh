bb316058a412c3ac46950b2b4a429607418511dc
Elvis Pranskevichus <elvis@edgedb.com>
2020-09-08 18:07:52 -0700

    Rudimentary Windows support
    This treats Windows as a Unix-ish target that has all the necessary GNU
    tools, Python, Git, Rust etc.
    
    Closes: #5

---
+++ integration/win/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -Exeo pipefail
+
+pip install -U git+https://github.com/edgedb/metapkg@win
+pip install -U git+https://github.com/edgedb/edgedb-pkg@win
+
+# Circumvent a dubious practice of Windows intercepting
+# bare invocations of "bash" to mean "WSL", since make
+# runs its shells using bare names even if SHELL contains
+# a fully-qualified path.
+shell_path=$(dirname "${SHELL}")
+cp -a "${SHELL}" "${shell_path}/realbash"
+
+extraopts=
+if [ -n "${SRC_REF}" ]; then
+    extraopts+=" --source-ref=${SRC_REF}"
+fi
+
+if [ -n "${PKG_VERSION}" ]; then
+    extraopts+=" --pkg-version=${PKG_VERSION}"
+fi
+
+if [ -n "${PKG_REVISION}" ]; then
+    if [ "${PKG_REVISION}" = "<current-date>" ]; then
+        PKG_REVISION="$(date -u +%Y%m%d%H)"
+    fi
+    extraopts+=" --pkg-revision=${PKG_REVISION}"
+fi
+
+if [ -n "${PKG_SUBDIST}" ]; then
+    extraopts+=" --pkg-subdist=${PKG_SUBDIST}"
+fi
+
+if [ -n "${BUILD_GENERIC}" ]; then
+    extraopts+=" --generic"
+fi
+
+dest="artifacts"
+if [ -n "${PKG_PLATFORM}" ]; then
+    dest+="/${PKG_PLATFORM}"
+fi
+
+if [ -n "${PKG_PLATFORM_VERSION}" ]; then
+    dest+="-${PKG_PLATFORM_VERSION}"
+fi
+
+if [ -z "${PACKAGE}" ]; then
+    PACKAGE="edgedbpkg.edgedb:EdgeDB"
+fi
+
+python -m metapkg build --dest="${dest}" ${extraopts} "${PACKAGE}"
+
+ls -al "${dest}"
