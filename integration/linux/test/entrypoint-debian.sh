92a42fccf245d41404c24f0fe747e93158d1bb8a
Fantix King <fantix.king@gmail.com>
2021-04-13 11:11:32 -0400

    Revert "Exclude test_server_ops_detect_postgres_pool_size on Debian targets"
    This reverts commit e89832b09ef7cc747e8becc79301efa6cc665666.

--- integration/linux/test/entrypoint-debian.sh
+++ integration/linux/test/entrypoint-debian.sh
@@ -41,7 +41,5 @@ apt-get install -y ./"${dest}"/edgedb-server-${slot}_*_amd64.deb
 su edgedb -c "/usr/lib/x86_64-linux-gnu/edgedb-server-${slot}/bin/python3 \
               -m edb.tools --no-devmode test \
               /usr/share/edgedb-server-${slot}/tests \
-              -e cqa_ -e tools_ \
-              -e test_server_ops_detect_postgres_pool_size \
-              --output-format=simple"
+              -e cqa_ -e tools_ --output-format=simple"
 echo "Success!"
