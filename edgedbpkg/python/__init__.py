2434988a1051fa6ceb8515fd12852b1ae2cb04e6
Elvis Pranskevichus <elvis@edgedb.com>
2022-03-31 10:56:13 -0700

    Bump zlib and libuuid

--- edgedbpkg/python/__init__.py
+++ edgedbpkg/python/__init__.py
@@ -45,8 +45,8 @@ class Python(packages.BundledPackage):
     bundle_deps = [
         openssl.OpenSSL("3.0.2"),
         libffi.LibFFI("3.4.2"),
-        libuuid.LibUUID("2.37"),
-        zlib.Zlib("1.2.11"),
+        libuuid.LibUUID("2.38"),
+        zlib.Zlib("1.2.12"),
     ]
 
     @classmethod
