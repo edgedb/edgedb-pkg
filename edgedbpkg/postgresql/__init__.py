1847290a48ee5d5cfc80c305bd0b89ac5946bd58
Elvis Pranskevichus <elvis@edgedb.com>
2023-02-13 21:13:24 -0800

    Bump OpenSSL

--- edgedbpkg/postgresql/__init__.py
+++ edgedbpkg/postgresql/__init__.py
@@ -46,7 +46,7 @@ class PostgreSQL(packages.BundledCPackage):
     bundle_deps = [
         icu.ICU("72.1"),
         libuuid.LibUUID("2.38"),
-        openssl.OpenSSL("3.0.7"),
+        openssl.OpenSSL("3.0.8"),
         zlib.Zlib("1.2.13"),
     ]
 
