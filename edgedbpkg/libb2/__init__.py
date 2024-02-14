5eaf94bfac6314191e860cfc040ce48d7b0db821
Elvis Pranskevichus <elvis@edgedb.com>
2024-02-13 22:06:33 -0800

    libb2: Actually fix it

--- edgedbpkg/libb2/__init__.py
+++ edgedbpkg/libb2/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pathlib
+
 from metapkg import packages
 from metapkg import targets
 
@@ -19,8 +21,13 @@ class LibB2(packages.BundledCPackage):
 
     def get_configure_script(self, build: targets.Build) -> str:
         sdir = build.get_source_dir(self, relative_to="pkgbuild")
+        # Restore modification times to avoid invoking the autotools.
+        script = (
+            f'touch -r "{sdir}/aclocal.m4" "{sdir}/configure.ac"'
+            f' "{sdir}/configure"\n'
+        )
         configure = sdir / "configure"
-        configure_flags = {
+        configure_flags: dict[str, str | pathlib.Path | None] = {
             "--disable-openmp": None,
             "--disable-native": None,
         }
@@ -28,7 +35,17 @@ class LibB2(packages.BundledCPackage):
             configure_flags["--enable-fat"] = None
         else:
             configure_flags["--disable-fat"] = None
-        return self.sh_configure(build, configure, configure_flags)
+
+        # Upstream defaults to -O3, so likely should we.
+        build.sh_append_flags(
+            configure_flags,
+            "CFLAGS",
+            (
+                "-O3",
+            ),
+        )
+
+        return script + self.sh_configure(build, configure, configure_flags)
 
     def get_shlibs(self, build: targets.Build) -> list[str]:
         return ["b2"]
